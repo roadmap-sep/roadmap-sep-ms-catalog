8aef24f121d374784f1b289907db7581b4aacd57
Ricardo Paez <ricardoyepes7@hotmail.com>
2026-08-18 16:50:46 -0500

    fix(CI): fix datasource test

--- src/test/java/sh/roadmap/sep/catalog/CatalogServiceApplicationTests.java
+++ src/test/java/sh/roadmap/sep/catalog/CatalogServiceApplicationTests.java
@@ -2,10 +2,12 @@ package sh.roadmap.sep.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CatalogServiceApplicationTests {
     @Test
     void contextLoads() {
